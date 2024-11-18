a626cd91610a6dbed42bf1cccc590d00f6a85793
Christopher Marquez <calvarez@motorway.co.uk>
2024-11-18 00:53:55 +0000

    feat: implement event loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,42 +5,45 @@ import java.net.Socket;
 
 public class Main {
 
-    private static void sendPong(Socket client) {
+    private static void sendPong(Socket clientSocket) {
         final var message = "+PONG\r\n";
-        try (var stream = client.getOutputStream()) {
+        try (var stream = clientSocket.getOutputStream()) {
             var writer = new PrintWriter(stream);
             writer.write(message);
             writer.flush();
         } catch (IOException e) {
             System.out.println("Shit");
         }
+    }
 
+    private static void acceptConnection(ServerSocket serverSocket) {
+        try (var clientSocket = serverSocket.accept()) {
+            sendPong(clientSocket);
+        } catch (IOException e) {
+            System.out.println("Client socket error");
+        }
+    }
+
+    private static ServerSocket initServer(int port) throws IOException {
+        var serverSocket = new ServerSocket(port);
+        serverSocket.setReuseAddress(true);
+        return serverSocket;
+    }
+
+    private static void listen(int port) {
+        try (var serverSocket = initServer(port)) {
+            System.out.println("Listening on port " + port);
+            while (true) {
+                acceptConnection(serverSocket);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
-        try {
-            serverSocket = new ServerSocket(port);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
-            serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            sendPong(clientSocket);
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
-        }
+        listen(port);
     }
 }
