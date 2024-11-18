acdeec6673e4fa03f05b5753e5786bb0303eb1bd
Christopher Marquez <calvarez@motorway.co.uk>
2024-11-18 00:43:44 +0000

    feat: implement naive pong

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+
+    private static void sendPong(Socket client) {
+        final var message = "+PONG\r\n";
+        try (var stream = client.getOutputStream()) {
+            var writer = new PrintWriter(stream);
+            writer.write(message);
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println("Shit");
+        }
+
+    }
+
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -16,6 +30,7 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+            sendPong(clientSocket);
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
