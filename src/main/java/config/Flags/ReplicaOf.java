bc99a44a2095f51a02628d88fa11684b8b0ad586
Christopher Marquez <calvarez@motorway.co.uk>
2024-11-26 18:45:12 +0000

    fix: fix replicaof parse

--- src/main/java/config/Flags/ReplicaOf.java
+++ src/main/java/config/Flags/ReplicaOf.java
@@ -5,16 +5,19 @@ import java.util.Arrays;
 public record ReplicaOf(String masterHost, int masterPort) implements ConfigFlag {
 
     private static String extractHost(String connectionString) {
-        final var params = Arrays.asList(connectionString.split("_"));
+        final var params = Arrays.asList(connectionString.split(" "));
         return params.getFirst();
     }
 
     private static int extractPort(String connectionString) {
-        final var params = Arrays.asList(connectionString.split("_"));
+        final var params = Arrays.asList(connectionString.split(" "));
         return Integer.parseInt(params.getLast());
     }
 
     public ReplicaOf(String connectionString) {
-        this(extractHost(connectionString), extractPort(connectionString));
+        this(
+            extractHost(connectionString),
+            extractPort(connectionString)
+        );
     }
 }
