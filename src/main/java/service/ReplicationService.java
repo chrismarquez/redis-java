b2a4d259331bfa675a7c09ca37e6e1ede80a10ce
Christopher Marquez <calvarez@motorway.co.uk>
2024-11-26 18:20:08 +0000

    feat: add hardcoded replication offset & id

--- src/main/java/service/ReplicationService.java
+++ src/main/java/service/ReplicationService.java
@@ -9,6 +9,9 @@ public class ReplicationService {
     private final Config config;
     private final String role;
 
+    private final String replicationID = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+    private final int replicationOffset = 0;
+
     public ReplicationService(Config config) {
         this.config = config;
         this.role = getRole();
@@ -20,7 +23,9 @@ public class ReplicationService {
 
     public Map<String, String> getReplicationInfo() {
         return Map.of(
-            "role", this.role
+            "role", this.role,
+            "master_replid", this.replicationID,
+            "master_repl_offset", Integer.toString(this.replicationOffset)
         );
     }
 
